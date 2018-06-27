7fbd8b250ed299c492a5bb698a08e4d8292a1295
Prakash Mishra <prakash.mishra@traveltriangle.com>
2018-06-27 16:31:20 +0530

    Base setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InventoryManagement::Application.config.secret_token = 'e4ec1a64d3cb6a89ace6aa8d0538d30201f38047d24feec8d1ee1d71c99426b009082e446f8a51bca7ce14b559ae92152d81ea43f4575443c15c48ede3cbecba'
